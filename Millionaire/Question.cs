17786c9c012cd10ee16922ec171885a63626d4be
JamesA <shub.ar@yandex.ru>
2019-07-29 16:42:16 +0300

    Added Question Constructor & changed wrongAnswers[] to wrongAnswer1,  wrongAnswer2...

--- Millionaire/Question.cs
+++ Millionaire/Question.cs
@@ -8,8 +8,29 @@ namespace Millionaire
 {
     public class Question
     {
-        public string questionText { get; set; }
-        public string[] wrongAnswers { get; } = new string[3];
-        public string rightAnswer { get; set; }
+        public Question()
+        {
+        }
+        public Question(IList<string> lines)
+        {
+            QuestionText = lines[0];
+            RightAnswer = lines[1];
+            WrongAnswer1 = lines[2];
+            WrongAnswer2 = lines[3];
+            WrongAnswer3 = lines[4];
+        }
+        public string QuestionText { get; set; }       
+        public string RightAnswer { get; set; }
+        public string WrongAnswer1 { get; set; }
+        public string WrongAnswer2 { get; set; }
+        public string WrongAnswer3 { get; set; }
+        public IEnumerable<string> GetQuestionData()
+        {
+            yield return QuestionText;
+            yield return RightAnswer;
+            yield return WrongAnswer1;
+            yield return WrongAnswer2;
+            yield return WrongAnswer3;
+        }
     }
 }
